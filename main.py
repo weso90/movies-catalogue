d4f5ff83a1e40f1f596faf4e8a7151f80e50cb06
Maksym Wesolowski <weso.pl@gmail.com>
2025-05-21 21:14:29 +0200

    Full app from exercise 10.4

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import tmdb_client
+import random
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(8)
-    return render_template("homepage.html", movies=movies)
+    movies_list = {
+        'now_playing': 'Now Playing',
+        'popular': 'Popular',
+        'top_rated': 'Top Rated',
+        'upcoming': 'Upcoming'
+    }
+    selected_list = request.args.get('list_type', 'upcoming')
+
+    if selected_list not in movies_list:
+        selected_list = 'popular'
+
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list, movies_list=movies_list)
 
 @app.context_processor
 def utility_processor():
     def tmdb_image_url(path, size):
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
+
+@app.route('/movie/<movie_id>')
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    casts = tmdb_client.get_single_movie_cast(movie_id)
+    movie_image = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_image["backdrops"])
+    return render_template("movie_details.html", movie=details, casts=casts, selected_backdrop=selected_backdrop)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,10 +1,12 @@
 import requests
+import random
+
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZWZkOTYzZjY1ODRiZDJkYzQyZDVjYzg0N2I0ZmQ2MCIsIm5iZiI6MTc0NzMzNjUwOS4wMDMsInN1YiI6IjY4MjYzZDNjMjE4NmQ1YWU3ZWFkYTQzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o8ManlL1HSBqt2w12oLaGvB46_jfjq_x-SbVXBGH3SE"
 
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZWZkOTYzZjY1ODRiZDJkYzQyZDVjYzg0N2I0ZmQ2MCIsIm5iZiI6MTc0NzMzNjUwOS4wMDMsInN1YiI6IjY4MjYzZDNjMjE4NmQ1YWU3ZWFkYTQzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o8ManlL1HSBqt2w12oLaGvB46_jfjq_x-SbVXBGH3SE"
     headers = {
-        "Authorization": f"Bearer {api_token}"
+        "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
@@ -12,7 +14,43 @@ def get_popular_movies():
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
+#Function returning poster from movie
 
-def get_movies(how_many):
-    data = get_popular_movies()
-    return data["results"][:how_many]
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
+    all_movies = data["results"]
+    return random.sample(all_movies, min(how_many, len(all_movies)))
+#Function returning random movies from all popular movies
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
